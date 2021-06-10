0d2b324d41254ab3c05e8a484a3ea204ddbd1834
Pooja Rao <raopooj@gmail.com>
2021-06-10 12:42:57 +1000

    testing button

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-//Validate form
+
+// newTaskTitle
+// newTaskDescription
+// newTaskDate
+// newTaskAssignTo
+// newTaskStatus
+// newTaskEasy
+// newTaskNormal
+// newTaskDard
+let newTaskTitle = document.body.querySelector("#newTaskTitle");
+let errMsgNewTask =  document.body.querySelector("#errMsgNewTask");
+let addTaskBtn = document.body.getElementById("#addTaskBtn");
+const btnTest = document.body.querySelector("#btnTest");
+//Creating task form validation
+const validFormFieldInput = () => {
+    console.log("test button");
+  if(newTaskTitle === "" || newTaskTitle.length < 4){
+    errMsgNewTask.innerHTML = "Your title should be 5 or more characters";
+  }
+}
+btnTest.addEventListener('click', validFormFieldInput);
