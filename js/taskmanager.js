ae0053df4645cf9244e72ba80b095bcb452f2360
notevenagoat <itinoco@pacificstream.com.au>
2021-06-15 15:40:40 +1000

    replaced experiment function with anonymous event listener. added the render call to replace the card with item

--- js/taskmanager.js
+++ js/taskmanager.js
@@ -1,20 +1,23 @@
+const sampleCard = document.querySelector("#sampleCard");
+
+
 const createTaskHtml = (taskTitle,taskDescription,taskAssignedTo,taskDueDate,taskStatus) => {
     const displaytask = `<li class="list-group-item">
     <div class="card text-center">
         <div class="card-header">
-          ${taskStatus}
+        ${taskStatus}
         </div>
         <div class="card-body">
           <h5 class="card-title">${taskTitle}</h5>
-        <h6 class="card-subtitle mb-2 text-muted"> ${taskAssignedTo}</h6>
+          <h6 class="card-subtitle mb-2 text-muted"> Assigned to ${taskAssignedTo}</h6>
           <p class="card-text">${taskDescription}  </p>
           <a href="#" class="btn btn-primary"> Modify</a>
         </div>
         <div class="card-footer text-muted">
-          ${taskDueDate}
+        ${taskDueDate}
         </div>
       </div>    
-    </li>`
+</li>`
     return displaytask;
 
 }
@@ -25,30 +28,42 @@ class TaskManager{
         this.taskId=[currentId]; 
         this.taskList = [];
     }
-    addTask(taskTitle,taskDescription,taskAssignedTo,taskDueDate,taskStatus,taskLevel){
+    addTask(taskTitle,taskDescription,taskAssignedTo,taskDueDate,taskStatus){
         this.taskId++;
+        let convertedStatus='';
+        if (taskStatus === '1'){
+          convertedStatus='To DO';
+        } else if (taskStatus === '2'){
+          convertedStatus='IN PROGRESS';
+        } else if (taskStatus === '3'){
+          convertedStatus='REVIEW';
+        } else if (taskStatus === '4'){
+          convertedStatus='DONE';
+        } 
         const sampleTask = {
             Id : this.taskId,
             Title: taskTitle,
             Description : taskDescription,
             AssignedTo : taskAssignedTo,
             DueDate : taskDueDate,
-            Status : taskStatus,
-            Level : taskLevel
+            Status : convertedStatus,
+  
         }
         this.taskList.push(sampleTask);
     }
     render(){
        let tasksHtmlLists = [];
+    
        for(let i=0; i< this.taskList.length; i++){
            let currentTask = this.taskList[i];
-           let dueDate = new Date(currentTask.taskDueDate);
-           let formattedDate = dueDate.format('dd-mm-yy');
-           let taskHtml = createTaskHtml(taskList.taskTitle, taskList.taskDescription, taskList.taskAssignedTo, formattedDate,taskList.taskStatus);
+           let currentdate = new Date(currentTask.DueDate);
+           let formattedDate =currentdate.getDate() + "/" + (currentdate.getMonth() + 1) + "/" + currentdate.getFullYear();
+           let taskHtml = createTaskHtml(currentTask.Title, currentTask.Description, currentTask.AssignedTo, formattedDate, currentTask.Status);
            tasksHtmlLists.push(taskHtml);
 
        }
        let taskHtml1 =  tasksHtmlLists.join("\n");
        sampleCard.innerHTML = taskHtml1;
-       
-}
+    }
+  }
+  
