ec483201b0c3a0579425055bf110788eeff469d4
notevenagoat <itinoco@pacificstream.com.au>
2021-06-28 17:37:22 +1000

    making code look a bit more  clean

--- js/index.js
+++ js/index.js
@@ -1,58 +1,62 @@
 
-//testing the function in taskmanger 
+// Creates taskmanager object, loads it and renders it
+
 let allTasks = new TaskManager();
 allTasks.load();
 allTasks.render();
-//Bootstrap form validation
+
+//Bootstrap form hiding
 var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+
+//Clearing the form
 
 const clearform = () => {
   document.querySelector("#newTaskTitle").value = "";
   document.querySelector("#newTaskDescription").value = "";
   document.querySelector("#newTaskDate").value = "";
-  document.querySelector("#newTaskAssignTo").value = "";
-  
+  document.querySelector("#newTaskAssignTo").value = "";  
 }
-(function () {
-    'use strict'
-    //console.log("Hi");
-    const forms = document.querySelectorAll('.requires-validation')
-    Array.from(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-          
-          else {
-          let newTaskTitle = document.querySelector("#newTaskTitle").value;
-          let newTaskDescription = document.querySelector("#newTaskDescription").value;
-          let newTaskDate = document.querySelector("#newTaskDate").value;
-          let newTaskAssignTo = document.querySelector("#newTaskAssignTo").value;
-          let newTaskStatus = document.querySelector("#newTaskStatus").value;
-      
-        let newTaskDiff = allTasks.checkButton();
-          allTasks.addTask(newTaskTitle, newTaskDescription, newTaskAssignTo, newTaskDate, newTaskStatus, newTaskDiff);
-          allTasks.render();
-          allTasks.save();
-                 event.preventDefault();
-                 clearform();
-                //  form.classList.remove('was-validated');
-                 myModal.hide();
-                 
-                 
-        }
-       form.classList.add('was-validated');
-      }, false)
-     }) 
-    })();
 
-let taskHtml = createTaskHtml('poject work', 'Work on task1', 'Pooja', '12/08/21', 'In Progress');
-console.log(taskHtml);
+// ADD TASK event listener
+
+(function () {
+            'use strict'
+            const forms = document.querySelectorAll('.requires-validation')
+            Array.from(forms)
+              .forEach(function (form) {
+                    form.addEventListener('submit', function (event) {
+                        if (!form.checkValidity()) {
+                          event.preventDefault()
+                          event.stopPropagation()
+                        } 
+                      else {
+                        let newTaskTitle = document.querySelector("#newTaskTitle").value;
+                        let newTaskDescription = document.querySelector("#newTaskDescription").value;
+                        let newTaskDate = document.querySelector("#newTaskDate").value;
+                        let newTaskAssignTo = document.querySelector("#newTaskAssignTo").value;
+                        let newTaskStatus = document.querySelector("#newTaskStatus").value;
+
+            // Check radio button for difficulty
+                        let newTaskDiff = allTasks.checkButton();
+
+              // Add tasks to objects, displays them, and saves them to local storage. 
+                        allTasks.addTask(newTaskTitle, newTaskDescription, newTaskAssignTo, newTaskDate, newTaskStatus, newTaskDiff);
+                        allTasks.render();
+                        allTasks.save();
+
+              // Stops refresh, clears the form, hides the pop up window
+                        event.preventDefault();
+                        clearform();
+                        myModal.hide();
+                            }
+                  form.classList.add('was-validated');
+                  }, false)
+                }) 
+            })();
 
 //Using query to assign pageList to the html bootstrap card list
 const pageList = document.querySelector(".sampleCard");
+
 //Checking if the done botton was clicked
 pageList.addEventListener('click', function (event) {
    let doneCheck = event.target.classList.contains('done-button');
